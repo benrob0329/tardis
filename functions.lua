b630fe5c4329d294a63f04cca7fadba01728f1cb
Benrob0329 <ben@innovationplex.com>
2017-04-04 21:58:09 -0400

    Redo code style

--- functions.lua
+++ functions.lua
@@ -1,58 +1,75 @@
-function tardis.swap_node(pos, name)
-   local meta = minetest.get_meta(pos)
-   local meta_old = meta:to_table()
-   
-   minetest.set_node(pos, name)
-   
-   local meta = minetest.get_meta(pos)
-   meta:from_table(meta_old)
+local modname   = minetest.get_current_modname()
+local modpath   = minetest.get_modpath(modname)
+local worldpath = minetest.get_worldpath()
+
+function tardis.swap_node (pos, name)
+    local meta     = minetest.get_meta (pos)
+    local meta_old = meta:to_table()
+
+    minetest.set_node (pos, name)
+
+    local meta     = minetest.get_meta (pos)
+    meta:from_table (meta_old)
 end
 
 
-function tardis:spawn_interior(pos, owner)
-   local place_pos = {x = pos["x"], y = 30000, z = pos["z"]}
-   local interior_doors_pos = { x = (place_pos["x"] + 5),y = (place_pos["y"] + 1),z = (place_pos["z"] +1) }
-   local demat_lever_pos = {x = (place_pos["x"] + 4) , y = (place_pos["y"] + 2) , z = (place_pos["z"] + 7) }
-   
-   minetest.place_schematic(place_pos, modpath .. "/schematics/tardis_interior.mts")
-   tardis.owners[owner] = {}
-   tardis.owners[owner]["exterior"] = pos
-   tardis.owners[owner]["interior"] = interior_doors_pos
-   tardis.owners[owner]["in_vortex"] = false
+function tardis:spawn_interior (pos, owner)
+    local place_pos = {
+        x = pos ["x"] ,
+        y = 30000    ,
+        z = pos ["z"] ,
+    }
+    local interior_doors_pos = {
+        x = (place_pos ["x"] + 5) ,
+        y = (place_pos ["y"] + 1) ,
+        z = (place_pos ["z"] + 1) ,
+    }
+    local demat_lever_pos = {
+        x = (place_pos ["x"] + 4) ,
+        y = (place_pos ["y"] + 2) ,
+        z = (place_pos ["z"] + 7) ,
+    }
 
-   local demat_meta = minetest.get_meta(demat_lever_pos)
-   demat_meta:set_string("owner", owner)
-   
-   local interior_doors_meta = minetest.get_meta(interior_doors_pos)
-   interior_doors_meta:set_string("owner", owner)
-   
-   print(minetest.pos_to_string(tardis.owners[owner]["interior"]))
+    minetest.place_schematic (place_pos, modpath .. "/schematics/tardis_interior.mts")
 
-   local file = io.open(worldpath .. "/tardis.owners", "w+")
-   file:write(minetest.serialize(tardis.owners))
-   file:close()
+    tardis.tardises [owner]              = {}
+    tardis.tardises [owner]["exterior"]  = pos
+    tardis.tardises [owner]["interior"]  = interior_doors_pos
+    tardis.tardises [owner]["in_vortex"] = false
+
+    local demat_meta = minetest.get_meta (demat_lever_pos)
+    demat_meta:set_string ("owner", owner)
+
+    local interior_doors_meta = minetest.get_meta (interior_doors_pos)
+    interior_doors_meta:set_string ("owner", owner)
+
+    minetest.log("info", minetest.pos_to_string (tardis.tardises [owner]["interior"] ))
+
+    local file = io.open (worldpath .. "/tardis.tardises", "w+")
+    file:write ( minetest.serialize (tardis.tardises) )
+    file:close()
 end
 
-function tardis.set_nav(player, owner)
-   local player_name = player:get_player_name()
-   
-   minetest.show_formspec(player_name, "tardis:remat_form",
-                          "size[4,3]" ..
-                             "field[1,1.5;3,1;coords;Co-ords;]" ..
-                             "button_exit[1,2;2,1;exit;Go!]")
-   
-   minetest.register_on_player_receive_fields(function(player, formname, fields)
-         if formname ~= "tardis:remat_form" then
+function tardis.set_nav (player, owner)
+    local player_name = player:get_player_name()
+
+    minetest.show_formspec (player_name, "tardis:remat_form",
+        "size[4,3]" ..
+        "field[1,1.5;3,1;coords;Co-ords;]" ..
+        "button_exit[1,2;2,1;exit;Go!]")
+
+    minetest.register_on_player_receive_fields (function (player, formname, fields)
+        if formname ~= "tardis:remat_form" then
             return false
-         end
+        end
 
-         local coords = minetest.string_to_pos(fields.coords)
+        local coords = minetest.string_to_pos (fields.coords)
 
-         if (coords == nil) then
-            minetest.chat_send_player(player_name, "Please Enter Valed Coords")
-         else
-            tardis.owners[owner]["destination"] = coords       
+        if (coords == nil) then
+            minetest.chat_send_player (player_name, "Please Enter Valed Coords")
+        else
+            tardis.tardises [owner]["destination"] = coords
             return true
-         end
-   end)
+        end
+    end)
 end
--- init.lua
+++ init.lua
@@ -1,36 +1,37 @@
 tardis = {}
-tardis.owners = {}
+tardis.tardises = {}
 
-modpath = minetest.get_modpath(minetest.get_current_modname())
-worldpath = minetest.get_worldpath()
+local modname   = minetest.get_current_modname()
+local modpath   = minetest.get_modpath(modname)
+local worldpath = minetest.get_worldpath()
 
 dofile(modpath .. "/remat.lua")
 dofile(modpath .. "/demat.lua")
 dofile(modpath .. "/functions.lua")
 dofile(modpath .. "/nodes.lua")
 
-local file = io.open(worldpath .. "/tardis.owners", "r")
+local file = io.open(worldpath .. "/tardis.tardises", "r")
 
 if file then
-   tardis.owners = minetest.deserialize(file:read("*all"))
-   file:close()
+    tardis.tardises = minetest.deserialize(file:read("*all"))
+    file:close()
 end
 
 minetest.register_chatcommand("set_nav", {
-                                 func = function(name, param)
-                                    if (tardis.owners[name] == nil) then
-                                       name.chat_send_player(name, "Must be owner!")
-                                    else
-                                       local owner = name
-                                       local player = minetest.get_player_by_name(name)
-                                       
-                                       tardis.set_nav(player, owner)
-                                    end
-                                 end
+    func = function(name, param)
+        if (tardis.tardises[name] == nil) then
+            name.chat_send_player(name, "Must be owner!")
+        else
+            local owner = name
+            local player = minetest.get_player_by_name(name)
+
+            tardis.set_nav(player, owner)
+        end
+    end
 })
 
 minetest.register_on_shutdown(function()
-      local file = io.open(worldpath .. "/tardis.owners", "w+")
-      file:write(minetest.serialize(tardis.owners))
-      file:close()
+    local file = io.open(worldpath .. "/tardis.tardises", "w+")
+    file:write(minetest.serialize(tardis.tardises))
+    file:close()
 end)
