d80f6247883ae10d8a9de286bf868b0c6793f280
Benrob0329 <ben@innovationplex.com>
2017-07-27 01:12:38 -0400

    Fix the indentation (Again)

--- nodes.lua
+++ nodes.lua
@@ -1,120 +1,120 @@
 minetest.register_node ("tardis:tardis", {
-    decription        = "Time And Relative Dimention In Space" ,
-    tiles             = { "tardis_exterior.png" } ,
-    use_texture_alpha = true ,
-    drawtype          = "mesh" ,
-    mesh              = "tardis_exterior.obj" ,
-    paramtype         = "light" ,
-    is_ground_content = true ,
+	decription        = "Time And Relative Dimention In Space" ,
+	tiles             = { "tardis_exterior.png" } ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-    on_place = function (itemstack, placer, pointed_thing)
-        local pos    = pointed_thing.above
-        local player = placer:get_player_name()
-        tardis.swap_node (pos, {name = "tardis:tardis"})
+	on_place = function (itemstack, placer, pointed_thing)
+		local pos    = pointed_thing.above
+		local player = placer:get_player_name()
+		tardis.swap_node (pos, {name = "tardis:tardis"})
 
-        if (player == nil) then
-            local meta   = minetest.get_meta (pos)
-            local player = meta:get_string ("owner")
-            tardis.tardises [player]["exterior"] = pos
-        elseif (tardis.tardises [player] == nil) then
-            tardis:spawn_interior (pos, player)
-        else
-            tardis.tardises [player]["exterior"] = pos
-        end
+		if (player == nil) then
+			local meta   = minetest.get_meta (pos)
+			local player = meta:get_string ("owner")
+			tardis.tardises [player]["exterior"] = pos
+		elseif (tardis.tardises [player] == nil) then
+			tardis:spawn_interior (pos, player)
+		else
+			tardis.tardises [player]["exterior"] = pos
+		end
 
-        local meta = minetest.get_meta (pos)
-        meta:set_string ("owner", player)
-        itemstack:take_item (1)
-    end ,
+		local meta = minetest.get_meta (pos)
+		meta:set_string ("owner", player)
+		itemstack:take_item (1)
+	end ,
 
 
-    on_rightclick = function (pos, node, player, itemstack, pointed_thing)
-        local meta     = minetest.get_meta (pos)
-        local owner    = meta:get_string ("owner")
-        local teleport = tardis.tardises [owner]["interior"]
+	on_rightclick = function (pos, node, player, itemstack, pointed_thing)
+		local meta     = minetest.get_meta (pos)
+		local owner    = meta:get_string ("owner")
+		local teleport = tardis.tardises [owner]["interior"]
 
-        player.setpos (player, teleport)
-        --player.set_look_horizontal(0)
-    end ,
+		player.setpos (player, teleport)
+		--player.set_look_horizontal(0)
+	end ,
 })
 
 
 minetest.register_node ("tardis:demat_lever_on", {
-    tiles             = {"tardis_demat.png"} ,
-    drawtype          = "mesh" ,
-    mesh              = "tardis_demat_on.obj" ,
-    paramtype         = "light" ,
-    is_ground_content = true ,
+	tiles             = {"tardis_demat.png"} ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_demat_on.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-    on_rightclick     = function (pos, node, player, itemstack, pointed_thing)
-        local player_name = player:get_player_name()
-        local meta        = minetest.get_meta(pos)
-        local owner       = meta:get_string("owner")
+	on_rightclick     = function (pos, node, player, itemstack, pointed_thing)
+		local player_name = player:get_player_name()
+		local meta        = minetest.get_meta(pos)
+		local owner       = meta:get_string("owner")
 
-        if (tardis.remat (owner) == false) then
-            minetest.chat_send_player (player_name, "Nav Not Set!!")
-        else
-            minetest.sound_play ("tardis_demat", {
-                pos               = pos ,
-                max_hear_distance = 10 ,
-                gain              = 10 ,
-            })
+		if (tardis.remat (owner) == false) then
+			minetest.chat_send_player (player_name, "Nav Not Set!!")
+		else
+			minetest.sound_play ("tardis_demat", {
+				pos               = pos ,
+				max_hear_distance = 10 ,
+				gain              = 10 ,
+			})
 
-            minetest.swap_node (pos, {name = "tardis:demat_lever_off"})
-        end
-    end
+			minetest.swap_node (pos, {name = "tardis:demat_lever_off"})
+		end
+	end
 })
 
 
 minetest.register_node ("tardis:demat_lever_off", {
-    groups            = {crumbly = 1} ,
-    tiles             = {"tardis_demat.png"} ,
-    drawtype          = "mesh" ,
-    mesh              = "tardis_demat_off.obj" ,
-    paramtype         = "light" ,
-    is_ground_content = true ,
+	groups            = {crumbly = 1} ,
+	tiles             = {"tardis_demat.png"} ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_demat_off.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-    on_rightclick     = function (pos, node, player, itemstack, pointed_thing)
-        local meta  = minetest.get_meta(pos)
-        local owner = meta:get_string("owner")
+	on_rightclick     = function (pos, node, player, itemstack, pointed_thing)
+		local meta  = minetest.get_meta(pos)
+		local owner = meta:get_string("owner")
 
-        tardis.demat (owner)
-        minetest.sound_play ("tardis_demat", {
-            pos               = pos ,
-            max_hear_distance = 10  ,
-            gain              = 10  ,
-        })
+		tardis.demat (owner)
+		minetest.sound_play ("tardis_demat", {
+			pos               = pos ,
+			max_hear_distance = 10  ,
+			gain              = 10  ,
+		})
 
-        minetest.swap_node (pos, {name = "tardis:demat_lever_on"})
-    end ,
+		minetest.swap_node (pos, {name = "tardis:demat_lever_on"})
+	end ,
 })
 
 
 minetest.register_node ("tardis:interior_doors", {
-    tiles             = {"tardis_exterior.png"} ,
-    use_texture_alpha = true ,
-    drawtype          = "mesh" ,
-    mesh              = "tardis_interior_doors.obj" ,
-    paramtype         = "light" ,
-    is_ground_content = true ,
+	tiles             = {"tardis_exterior.png"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_interior_doors.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-    on_rightclick = function (pos, node, player, itemstack, pointed_thing)
-        local meta = minetest.get_meta (pos)
-        local owner = meta:get_string ("owner")
+	on_rightclick = function (pos, node, player, itemstack, pointed_thing)
+		local meta = minetest.get_meta (pos)
+		local owner = meta:get_string ("owner")
 
-        if (tardis.tardises == nil) or
-           (tardis.tardises [owner] == nil) or
-           (tardis.tardises [owner]["in_vortex"] == nil) then
-               minetest.log ("error", "in_vortex not initialised for  " .. owner)
-               return
-        end
+		if (tardis.tardises == nil) or
+			(tardis.tardises [owner] == nil) or
+			(tardis.tardises [owner]["in_vortex"] == nil) then
+			minetest.log ("error", "in_vortex not initialised for  " .. owner)
+			return
+		end
 
-        if (tardis.tardises [owner]["in_vortex"] == false) then
-            local teleport = tardis.tardises [owner]["exterior"]
-            player:setpos (teleport)
-        else
-            player_name = player:get_player_name()
-            minetest.chat_send_player (player_name, "TARDIS In Vortex, Door Locked Automaticly")
-        end
-    end
+		if (tardis.tardises [owner]["in_vortex"] == false) then
+			local teleport = tardis.tardises [owner]["exterior"]
+			player:setpos (teleport)
+		else
+			player_name = player:get_player_name()
+			minetest.chat_send_player (player_name, "TARDIS In Vortex, Door Locked Automaticly")
+		end
+	end
 })
--- remat.lua
+++ remat.lua
@@ -1,202 +1,202 @@
 function tardis.remat (owner)
-		if (tardis.tardises [owner]["destination"] == nil) then
-				return false
-		else
-				local pos = tardis.tardises [owner]["destination"]
+	if (tardis.tardises [owner]["destination"] == nil) then
+		return false
+	else
+		local pos = tardis.tardises [owner]["destination"]
 
-				minetest.forceload_block (pos)
-				minetest.set_node (pos, {name = "tardis:tardis_remat"})
+		minetest.forceload_block (pos)
+		minetest.set_node (pos, {name = "tardis:tardis_remat"})
 
-				local meta = minetest.get_meta (pos)
-				meta:set_string ("owner", owner)
+		local meta = minetest.get_meta (pos)
+		meta:set_string ("owner", owner)
 
-				tardis.tardises [owner]["exterior"] = pos
+		tardis.tardises [owner]["exterior"] = pos
 
-				minetest.sound_play ("tardis_remat", {
-						pos               = pos ,
-						max_hear_distance = 100 ,
-						gain              = 10  ,
-				})
-		end
+		minetest.sound_play ("tardis_remat", {
+			pos               = pos ,
+			max_hear_distance = 100 ,
+			gain              = 10  ,
+		})
+	end
 end
 
 
 minetest.register_node ("tardis:tardis_remat", {
-		tiles             = {"tardis_exterior.png^[opacity:1"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
+	tiles             = {"tardis_exterior.png^[opacity:1"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_1"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_1"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(11)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(11)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_1", {
-		tiles             = {"tardis_exterior.png^[opacity:25"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
-		light_source      = 7 ,
+	tiles             = {"tardis_exterior.png^[opacity:25"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
+	light_source      = 7 ,
 
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_2"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_2"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1.5)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1.5)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_2", {
-		tiles             = {"tardis_exterior.png^[opacity:50"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
+	tiles             = {"tardis_exterior.png^[opacity:50"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_3"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_3"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_3", {
-		tiles             = {"tardis_exterior.png^[opacity:75"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
-		light_source      = 10 ,
+	tiles             = {"tardis_exterior.png^[opacity:75"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
+	light_source      = 10 ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_4"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_4"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_4", {
-		tiles             = {"tardis_exterior.png^[opacity:100"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
+	tiles             = {"tardis_exterior.png^[opacity:100"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_5"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_5"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(0.5)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(0.5)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_5", {
-		tiles             = {"tardis_exterior.png^[opacity:125"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
-		light_source      = 10 ,
+	tiles             = {"tardis_exterior.png^[opacity:125"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
+	light_source      = 10 ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_6"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_6"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_6", {
-		tiles             = {"tardis_exterior.png^[opacity:150"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
+	tiles             = {"tardis_exterior.png^[opacity:150"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_7"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_7"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_7", {
-		tiles             = {"tardis_exterior.png^[opacity:175"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		light_source      = 10 ,
+	tiles             = {"tardis_exterior.png^[opacity:175"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	light_source      = 10 ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_8"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_8"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_8", {
-		tiles             = {"tardis_exterior.png^[opacity:200"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
+	tiles             = {"tardis_exterior.png^[opacity:200"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis_remat_9"})
-		end ,
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis_remat_9"})
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(0.5)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(0.5)
+	end ,
 })
 
 minetest.register_node ("tardis:tardis_remat_9", {
-		tiles             = {"tardis_exterior.png^[opacity:225"} ,
-		use_texture_alpha = true ,
-		drawtype          = "mesh" ,
-		mesh              = "tardis_exterior.obj" ,
-		paramtype         = "light" ,
-		is_ground_content = true ,
-		light_source      = 10 ,
+	tiles             = {"tardis_exterior.png^[opacity:225"} ,
+	use_texture_alpha = true ,
+	drawtype          = "mesh" ,
+	mesh              = "tardis_exterior.obj" ,
+	paramtype         = "light" ,
+	is_ground_content = true ,
+	light_source      = 10 ,
 
-		on_timer = function (pos)
-				tardis.swap_node (pos, {name = "tardis:tardis"})
+	on_timer = function (pos)
+		tardis.swap_node (pos, {name = "tardis:tardis"})
 
-				local meta = minetest.get_meta (pos)
-				owner = meta:get_string ("owner")
+		local meta = minetest.get_meta (pos)
+		owner = meta:get_string ("owner")
 
-				tardis.tardises [owner]["in_vortex"] = false
-		end ,
+		tardis.tardises [owner]["in_vortex"] = false
+	end ,
 
-		on_construct = function (pos)
-				minetest.get_node_timer (pos):start(1)
-		end ,
+	on_construct = function (pos)
+		minetest.get_node_timer (pos):start(1)
+	end ,
 })
